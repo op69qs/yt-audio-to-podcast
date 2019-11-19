31052eb7199d5b947e47a9ca0313f1f7a5e8dc1d
op69qs <L7Itt9N1VfByhUEs>
2019-11-19 15:38:30 +0800

    replace youtube download function

--- server.js
+++ server.js
@@ -1,65 +1,69 @@
 const config = require('nodejs-config')(
-	__dirname
+    __dirname
 );
 const ytAudioToPodcast = require('./youtube-audio-to-podcast.js');
 
 const http = require("http");
-const server = http.createServer(function(request, response) {
-	// Parse out the YT channel name
-	// Format : /[YouTube channel name]/feed.xml
-	const urlParts = request.url.split('/');
-	const channel = urlParts[1];
-	const filename = urlParts[2];
-	const mp3FileMatch = (filename || '').match(/(.*)\.mp3$/);
+const server = http.createServer(function (request, response) {
+    // Parse out the YT channel name
+    // Format : /[YouTube channel name]/feed.xml
+    const urlParts = request.url.split('/');
+    const channel = urlParts[1];
+    const filename = urlParts[2];
+    const mp3FileMatch = (filename || '').match(/(.*)\.mp3$/);
 
-	if (filename === 'feed.xml' || filename === 'user.xml') {
-		response.writeHead(200, {"Content-Type": "application/rss+xml;charset=utf-8"});
+    if (filename === 'feed.xml' || filename === 'user.xml') {
+        response.writeHead(200, {"Content-Type": "application/rss+xml;charset=utf-8"});
 
-		ytAudioToPodcast.getPodcastRssXmlByUsername(
-			channel, 
-			request.url,
-			'http://'+request.headers.host+'/'+channel+'/[videoId].mp3',
-			function(xml) {
-				response.write(xml);
-				response.end();
-			}
-		);
-	} else if (filename === 'channel.xml') {
-		response.writeHead(200, {"Content-Type": "application/rss+xml;charset=utf-8"});
+        ytAudioToPodcast.getPodcastRssXmlByUsername(
+            channel,
+            request.url,
+            'http://' + request.headers.host + '/' + channel + '/[videoId].mp3',
+            function (xml) {
+                response.write(xml);
+                response.end();
+            }
+        );
+    } else if (filename === 'channel.xml') {
+        response.writeHead(200, {"Content-Type": "application/rss+xml;charset=utf-8"});
 
-		ytAudioToPodcast.getPodcastRssXmlByChannelId(
-			channel, 
-			request.url,
-			'http://'+request.headers.host+'/'+channel+'/[videoId].mp3',
-			function(xml) {
-				response.write(xml);
-				response.end();
-			}
-		);
-	} else if (mp3FileMatch) {
+        ytAudioToPodcast.getPodcastRssXmlByChannelId(
+            channel,
+            request.url,
+            'http://' + request.headers.host + '/' + channel + '/[videoId].mp3',
+            function (xml) {
+                response.write(xml);
+                response.end();
+            }
+        );
+    } else if (mp3FileMatch) {
         const videoId = mp3FileMatch[1];
-        const size = ytAudioToPodcast.getAudioCacheFileSize(videoId);
-        let header = {
-            "Content-Type": "audio/mpeg",
-            'Transfer-Encoding': 'chunked',
-            'connection': 'keep-alive',
-            'Content-Transfer-Encoding': 'binary',
-        };
-        if (size > 0) {
-            header['Content-Length'] = size
-        }
-        response.writeHead(200, header);
 
-		const audioStream = ytAudioToPodcast.getAudioStreamByVideoId(videoId);
-		audioStream.pipe(response);
-	} else {
-		response.writeHead(404);
-		response.write('Invalid request');
-		response.end();
-	}
+        ytAudioToPodcast.getAudioStreamByVideoId(videoId, function (stream, size, err) {
+            if (err) {
+                response.writeHead(500);
+                response.write(err);
+                response.end();
+            } else {
+                let header = {
+                    "Content-Type": "audio/mpeg",
+                    'Transfer-Encoding': 'chunked',
+                    'connection': 'keep-alive',
+                    'Content-Transfer-Encoding': 'binary',
+                    'Content-Length': size
+                };
+                response.writeHead(200, header);
+                stream.pipe(response);
+            }
+        });
+    } else {
+        response.writeHead(404);
+        response.write('Invalid request');
+        response.end();
+    }
 });
 
 const port = config.get("local.port");
-ytAudioToPodcast.audioSavePathInit(config.get("local.audioSavePath"))
+ytAudioToPodcast.audioSavePathInit(config.get("local.audioSavePath"));
 server.listen(port);
-console.log("Listening on "+port);
+console.log("Listening on " + port);
--- youtube-audio-to-podcast.js
+++ youtube-audio-to-podcast.js
@@ -1,184 +1,216 @@
 const fs = require("fs")
-const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
+const Queue = require('bull');
+const audioQueue = new Queue('audio transcoding', {
+    redis: {port: 6379, host: '127.0.0.1'},
+    limiter: {
+        max: 10
+    }
+}); // Specify Redis connection using object
+
+const {exec} = require('child_process');
+
+
 const config = require('nodejs-config')(
-	__dirname
+    __dirname
 );
-var m = module.exports = {};
+const m = module.exports = {};
 
-m.getPodcastItemsByChannelId = function(channelId, enclosureUrlTpl, cb) {
-	const {google} = require('googleapis');
+m.getPodcastItemsByChannelId = function (channelId, enclosureUrlTpl, cb) {
+    const {google} = require('googleapis');
 
-	const _ = require('underscore');
-	const yt = google.youtube('v3');
+    const _ = require('underscore');
+    const yt = google.youtube('v3');
 
-	yt.search.list({
-		part: 'id,snippet',
-		channelId: channelId,
-		key: config.get('local.youtubeApiKey'),
-		maxResults: 50,
-		order: 'date', // descending date order is important for a podcast
-		type: 'video',
-	}, function(err, list) {
-		// Pull out the relevant items for our podcast RSS
-		var items = list.data.items;
+    yt.search.list({
+        part: 'id,snippet',
+        channelId: channelId,
+        key: config.get('local.youtubeApiKey'),
+        maxResults: 50,
+        order: 'date', // descending date order is important for a podcast
+        type: 'video',
+    }, function (err, list) {
+        // Pull out the relevant items for our podcast RSS
+        let items = list.data.items;
 
-		// Map into our desired format
-		
-		items = _.map(_.filter(items,function(item){
-			if(item.snippet.liveBroadcastContent!=='live'){
-				return items
-			}
-		}), function(item) {
-			// sync to save audio
-			saveAudioFile(item.id.videoId);
-			
-			return {
-				title: item.snippet.title,
-				description: item.snippet.description,
-				url: 'http://youtu.be/'+item.id.videoId,
-				date: item.snippet.publishedAt,
-				enclosure: {
-					url: enclosureUrlTpl.replace('[videoId]', item.id.videoId),
-					type: 'audio/mpeg',
-				}
-			};
-		});
+        // Map into our desired format
 
-		
-		cb(items);
-	});
-}
+        items = _.map(_.filter(items, function (item) {
+            if (item.snippet.liveBroadcastContent !== 'live') {
+                return items
+            }
+        }), function (item) {
+            // sync to save audio
+            saveAudioFile(item.id.videoId);
 
-m.getChannelInfoById = function(channelId, feedUrl, cb) {
-	var {google} = require('googleapis');
-	var yt = google.youtube('v3');
+            return {
+                title: item.snippet.title,
+                description: item.snippet.description,
+                url: 'http://youtu.be/' + item.id.videoId,
+                date: item.snippet.publishedAt,
+                enclosure: {
+                    url: enclosureUrlTpl.replace('[videoId]', item.id.videoId),
+                    type: 'audio/mpeg',
+                }
+            };
+        });
 
-	return yt.channels.list({
-		part: 'id,snippet',
-		id: channelId,
-		key: config.get('local.youtubeApiKey')
-	}, function(err, list) {
-		var channel = list.data.items[0];
-		var ret = {};
 
-		if (!channel) return;
+        cb(items);
+    });
+};
 
-		ret[channel.id] = {
-			title: channel.snippet.title,
-			description: channel.snippet.description,
-			feed_url: feedUrl,
-			site_url: "http://youtube.com/channel/"+channel.id,
-			image_url: channel.snippet.thumbnails.default.url,
-		};
-		cb(ret);
-	});
-}
+m.getChannelInfoById = function (channelId, feedUrl, cb) {
+    const {google} = require('googleapis');
+    const yt = google.youtube('v3');
 
-m.getChannelInfoByUsername = function(username, feedUrl, cb) {
-	var {google} = require('googleapis');
-	var yt = google.youtube('v3');
+    return yt.channels.list({
+        part: 'id,snippet',
+        id: channelId,
+        key: config.get('local.youtubeApiKey')
+    }, function (err, list) {
+        const channel = list.data.items[0];
+        const ret = {};
 
-	return yt.channels.list({
-		part: 'id,snippet',
-		forUsername: username,
-		key: config.get('local.youtubeApiKey')
-	}, function(err, list) {
-		var channel = list.data.items[0];
-		var ret = {};
+        if (!channel) return;
 
-		if (!channel) return;
+        ret[channel.id] = {
+            title: channel.snippet.title,
+            description: channel.snippet.description,
+            feed_url: feedUrl,
+            site_url: "http://youtube.com/channel/" + channel.id,
+            image_url: channel.snippet.thumbnails.default.url,
+        };
+        cb(ret);
+    });
+};
 
-		ret[channel.id] = {
-			title: channel.snippet.title,
-			description: channel.snippet.description,
-			feed_url: feedUrl,
-			site_url: "http://youtube.com/channel/"+channel.id,
-			image_url: channel.snippet.thumbnails.default.url,
-		};
-		cb(ret);
-	});
-}
+m.getChannelInfoByUsername = function (username, feedUrl, cb) {
+    const {google} = require('googleapis');
+    const yt = google.youtube('v3');
 
-m.getPodcastRssXml = function(info, items) {
-	var rss = require('rss');
-	var _ = require('underscore');
+    return yt.channels.list({
+        part: 'id,snippet',
+        forUsername: username,
+        key: config.get('local.youtubeApiKey')
+    }, function (err, list) {
+        var channel = list.data.items[0];
+        var ret = {};
 
-	var feed = new rss(info);
-	_.each(items, function(item) {
-		feed.item(item);
-	});
+        if (!channel) return;
 
-	return feed.xml();
-}
+        ret[channel.id] = {
+            title: channel.snippet.title,
+            description: channel.snippet.description,
+            feed_url: feedUrl,
+            site_url: "http://youtube.com/channel/" + channel.id,
+            image_url: channel.snippet.thumbnails.default.url,
+        };
+        cb(ret);
+    });
+};
 
-m.getPodcastRssXmlByUsername = function(username, feedUrl, enclosureUrlTpl, cb) {
-	m.getChannelInfoByUsername(username, feedUrl, function(info) {
-		// Get the channel id
-		for (var channelId in info) { continue; }
+m.getPodcastRssXml = function (info, items) {
+    const rss = require('rss');
+    const _ = require('underscore');
 
-		m.getPodcastItemsByChannelId(channelId, enclosureUrlTpl, function(items) {
-			cb(m.getPodcastRssXml(info[channelId], items));
-		});
-	});
-}
+    const feed = new rss(info);
+    _.each(items, function (item) {
+        feed.item(item);
+    });
 
-m.getPodcastRssXmlByChannelId = function(channelId, feedUrl, enclosureUrlTpl, cb) {
-	m.getChannelInfoById(channelId, feedUrl, function(info) {
-		// Get the channel id
-		for (var channelId in info) { continue; }
+    return feed.xml();
+};
 
-		m.getPodcastItemsByChannelId(channelId, enclosureUrlTpl, function(items) {
-			cb(m.getPodcastRssXml(info[channelId], items));
-		});
-	});
-}
+m.getPodcastRssXmlByUsername = function (username, feedUrl, enclosureUrlTpl, cb) {
+    m.getChannelInfoByUsername(username, feedUrl, function (info) {
+        // Get the channel id
+        for (var channelId in info) {
+            continue;
+        }
 
-m.getAudioStreamByVideoId = function (videoId, outputStream) {
-	const vidUrl = 'https://www.youtube.com/watch?v=' + videoId;
-	const audioSavePath = __dirname + path.sep + config.get('local.audioSavePath') + path.sep + videoId + '.mp3';
-	if (!fs.existsSync(audioSavePath)) {
-		saveAudioFile(videoId);
-		console.log(new Date().toLocaleString() + " use online to stream: " + vidUrl);
-		let videoStream = ytdl(vidUrl);
-		return ffmpeg().input(videoStream).withAudioBitrate('48').format('mp3').pipe();
-	} else {
-		console.log(new Date().toLocaleString() + " use cache file to stream: " + vidUrl);
-		return fs.createReadStream(audioSavePath);
-	}
-	// let videoStream = ytdl(vidUrl, {filter: 'audioonly'});
+        m.getPodcastItemsByChannelId(channelId, enclosureUrlTpl, function (items) {
+            cb(m.getPodcastRssXml(info[channelId], items));
+        });
+    });
+};
+
+m.getPodcastRssXmlByChannelId = function (channelId, feedUrl, enclosureUrlTpl, cb) {
+    m.getChannelInfoById(channelId, feedUrl, function (info) {
+        // Get the channel id
+        for (var channelId in info) {
+            continue;
+        }
+
+        m.getPodcastItemsByChannelId(channelId, enclosureUrlTpl, function (items) {
+            cb(m.getPodcastRssXml(info[channelId], items));
+        });
+    });
+};
+
+m.getAudioStreamByVideoId = function (videoId, cb) {
+    const vidUrl = 'https://www.youtube.com/watch?v=' + videoId;
+    const audioSavePath = __dirname + path.sep + config.get('local.audioSavePath') + path.sep + videoId + '.mp3';
+    if (!fs.existsSync(audioSavePath)) {
+        saveAudioFile(videoId, cb);
+        // console.log(new Date().toLocaleString() + " use online to stream: " + vidUrl);
+        // let videoStream = ytdl(vidUrl);
+        // ffmpeg().input(videoStream).withAudioBitrate('48').format('mp3').pipe();
+    } else {
+        console.log(new Date().toLocaleString() + " use cache file to stream: " + vidUrl);
+        cb(fs.createReadStream(audioSavePath), fs.statSync(audioSavePath).size)
+    }
+    // let videoStream = ytdl(vidUrl, {filter: 'audioonly'});
 };
 
 m.getAudioCacheFileSize = function (videoId) {
-	const audioSavePath = __dirname + path.sep + config.get('local.audioSavePath') + path.sep + videoId + '.mp3';
-	if (!fs.existsSync(audioSavePath)) {
-		return 0;
-	} else {
-		return fs.statSync(audioSavePath).size;
-	}
+    const audioSavePath = __dirname + path.sep + config.get('local.audioSavePath') + path.sep + videoId + '.mp3';
+    if (!fs.existsSync(audioSavePath)) {
+        return 0;
+    } else {
+        return fs.statSync(audioSavePath).size;
+    }
 };
 
-saveAudioFile = async function (videoId) {
-	const audioSavePath = __dirname + path.sep + config.get('local.audioSavePath') + path.sep + videoId + '.mp3';
-	const vidUrl = 'https://www.youtube.com/watch?v=' + videoId;
-	// console.log("ytb url:" + vidUrl);
-	// let videoStream = ytdl(vidUrl, {filter: 'audioonly'});
-	if (!fs.existsSync(audioSavePath)) {
-		console.log(new Date().toLocaleString() + " start download job: " + vidUrl);
-		let videoStream = ytdl(vidUrl);
-		ffmpeg().input(videoStream).withAudioBitrate('48').format('mp3')
-			.on('error', function (err) {
-				console.log(new Date().toLocaleString() +" " +videoId + ' An error occurred: ' + err.message);
-			})
-			.save(audioSavePath);
-	}
+saveAudioFile = async function (videoId, cb) {
+    const audioSavePath = __dirname + path.sep + config.get('local.audioSavePath') + path.sep + videoId + '.mp3';
+    const vidUrl = 'https://www.youtube.com/watch?v=' + videoId;
+    // console.log("ytb url:" + vidUrl);
+    // let videoStream = ytdl(vidUrl, {filter: 'audioonly'});
+    if (!fs.existsSync(audioSavePath)) {
+        console.log(new Date().toLocaleString() + " start download job: " + vidUrl);
+        // let videoStream = ytdl(vidUrl);
+        //use system cmd
+        // console.log(stdout);
+        // ffmpeg().input(videoStream).withAudioBitrate('48').format('mp3')
+        //     .on('error', function (err) {
+        //         console.log(new Date().toLocaleString() + " " + videoId + ' An error occurred: ' + err.message);
+        //     }).on('end', function () {
+        //     if (cb && typeof cb === "function") {
+        //         cb(fs.createReadStream(audioSavePath), fs.statSync(audioSavePath).size)
+        //     }
+        // }).save(audioSavePath);
+
+        exec("youtube-dl -f 'worstaudio' '" + vidUrl + "' -o " + audioSavePath, function (err, stdout, stderr) {
+            if (err) {
+                console.error(new Date().toLocaleString() + " " + stdout);
+                cb(null, null, err)
+            } else {
+                if (cb && typeof cb === "function") {
+                    cb(fs.createReadStream(audioSavePath), fs.statSync(audioSavePath).size, err);
+                }
+            }
+        });
+
+
+    }
 };
 
 m.audioSavePathInit = function (dir) {
-	const audioSavePath = __dirname + path.sep + dir;
-	if (!fs.existsSync(audioSavePath)) {
-		fs.mkdirSync(audioSavePath);
-	}
-	console.log("audio save path init success")
+    const audioSavePath = __dirname + path.sep + dir;
+    if (!fs.existsSync(audioSavePath)) {
+        fs.mkdirSync(audioSavePath);
+    }
+    console.log("audio save path init success")
 };
